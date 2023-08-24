dfeca636982747af8e82113ad3f8effee9137f1a
Hanz <haniel56@zoho.eu>
2023-08-24 17:14:36 +0530

    chore: linux fix 2

--- flake.nix
+++ flake.nix
@@ -26,11 +26,167 @@
 
   outputs = inputs: let
     pkgsFor = system: import inputs.nixpkgs {inherit system;};
+
+    # linux configs
+    mkLinux = system:
+      pkgsFor system.lib.nixosSystem {
+        inherit system;
+        config.allowUnfree = true;
+
+        modules = [
+          inputs.home-manager.nixosModules.home-manager
+          {
+            users.users.hanz.home = "/home/hanz";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              users.hanz.imports = [
+                ({pkgs, ...}: {
+                  home.username = "hanz";
+                  home.homeDirectory = pkgs.lib.mkDefault "/home/hanz";
+
+                  ### Don't change this when you change package input. Leave it alone. ###
+                  home.stateVersion = "23.05";
+                  ### --- ###
+
+                  home.packages = with pkgs; [
+                    ripgrep
+                    fd
+                    curl
+                    less
+                    ncdu
+                    neofetch
+                    htop
+                    gh
+                    lazygit
+                    tealdeer # fast tldr
+                    alejandra # nix formatter
+                    protobuf # protobuf compiler
+                    nodejs-18_x
+                    nodePackages.pnpm
+
+                    curlie
+                    trippy # network diagnostics tool
+                  ];
+
+                  home.sessionVariables = {
+                    PAGER = "less";
+                    CLICLOLOR = 1;
+                    EDITOR = "nvim";
+                  };
+
+                  environment.systemPackages = [
+                    pkgs.fish
+                    pkgs.rustup
+                    pkgs.tailscale
+                  ];
+
+                  environment.shells = [pkgs.bash pkgs.fish];
+                  environment.loginShell = pkgs.fish;
+
+                  services.tailscale.enable = true;
+
+                  system.keyboard.enableKeyMapping = true;
+                  system.keyboard.remapCapsLockToEscape = true;
+
+                  # Auto upgrade nix package and the daemon service.
+                  services.nix-daemon.enable = true;
+
+                  # extra nix configs
+                  nix.settings.auto-optimise-store = true;
+                  nix.extraOptions = ''
+                    auto-optimise-store = true
+                    experimental-features = nix-command flakes
+                  '';
+
+                  # programs to install
+
+                  programs.neovim = {
+                    enable = true;
+                    viAlias = true;
+                    vimAlias = true;
+                    # extraPackages = [gcc];
+                  };
+
+                  programs.zoxide = {
+                    enable = true; # smarter 'cd'
+                    enableFishIntegration = true;
+                  };
+
+                  programs.bat.enable = true;
+                  programs.fzf.enable = true;
+                  programs.fzf.enableFishIntegration = true;
+                  programs.lsd.enable = true;
+                  programs.lsd.enableAliases = true;
+
+                  programs.fish.enable = true;
+                  programs.fish.shellAliases = {
+                    cd = "z";
+                    zj = "zellij";
+                    lg = "lazygit";
+                    gs = "git status -s";
+                    b = "bat";
+                    n = "nvim";
+                    vim = "nvim";
+                    curl = "curlie";
+                  };
+
+                  # Let Home Manager install and manage itself.
+                  programs.home-manager.enable = true;
+
+                  # configure git
+                  programs.git = {
+                    enable = true;
+                    userName = "Hanz";
+                    userEmail = "haniel56@zoho.eu";
+
+                    ignores = [
+                      ".direnv"
+                    ];
+
+                    extraConfig = {
+                      pull.rebase = true;
+                      init.defaultBranch = "main";
+                      github.user = "fivehanz";
+                    };
+                  };
+
+                  programs.direnv = {
+                    enable = true;
+                    nix-direnv.enable = true;
+                  };
+
+                  programs.starship = let
+                    flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
+                  in {
+                    enable = true;
+                    enableFishIntegration = true;
+
+                    settings =
+                      {
+                        # Other config here
+                        format = "$all"; # Remove this line to disable the default prompt format
+                        palette = "catppuccin_${flavour}";
+                      }
+                      // builtins.fromTOML (builtins.readFile
+                        (pkgs.fetchFromGitHub
+                          {
+                            owner = "catppuccin";
+                            repo = "starship";
+                            rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc"; # Replace with the latest commit hash
+                            sha256 = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
+                          }
+                          + /palettes/${flavour}.toml));
+                  };
+                })
+              ];
+            };
+          }
+        ];
+      };
   in {
     formatter.aarch64-darwin = inputs.nixpkgs.legacyPackages.aarch64-darwin.alejandra;
-    formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.aarch64-darwin.alejandra;
-    formatter.aarch64-linux = inputs.nixpkgs.legacyPackages.aarch64-darwin.alejandra;
-
     darwinConfigurations = {
       mkDarwin = inputs.darwin.lib.darwinSystem {
         system = "aarch64-darwin";
@@ -246,182 +402,8 @@
     };
 
     linuxConfigurations = {
-      mkLinuxVM = inputs.nixpkgs.lib.nixosSystem {
-        inherit (pkgsFor "x86_64-linux") system;
-
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          inputs.home-manager.nixosModules.home-manager
-          {
-            users.users.hanz.home = "/home/hanz";
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-
-              users.hanz.imports = [
-                ({pkgs, ...}: {
-                  home.username = "hanz";
-                  home.homeDirectory = pkgs.lib.mkDefault "/home/hanz";
-
-                  ### Don't change this when you change package input. Leave it alone. ###
-                  home.stateVersion = "23.05";
-                  ### --- ###
-
-                  home.packages = with pkgs; [
-                    ripgrep
-                    fd
-                    curl
-                    less
-                    ncdu
-                    neofetch
-                    htop
-                    gh
-                    lazygit
-                    tealdeer # fast tldr
-                    alejandra # nix formatter
-                    protobuf # protobuf compiler
-                    nodejs-18_x
-                    nodePackages.pnpm
-
-                    curlie
-                    trippy # network diagnostics tool
-                  ];
-
-                  home.sessionVariables = {
-                    PAGER = "less";
-                    CLICLOLOR = 1;
-                    EDITOR = "nvim";
-                  };
-
-                  environment.systemPackages = [
-                    pkgs.fish
-                    pkgs.rustup
-                    pkgs.tailscale
-                  ];
-
-                  environment.shells = [pkgs.bash pkgs.fish];
-                  environment.loginShell = pkgs.fish;
-
-                  services.tailscale.enable = true;
-
-                  system.keyboard.enableKeyMapping = true;
-                  system.keyboard.remapCapsLockToEscape = true;
-
-                  # Auto upgrade nix package and the daemon service.
-                  services.nix-daemon.enable = true;
-
-                  # extra nix configs
-                  nix.settings.auto-optimise-store = true;
-                  nix.extraOptions = ''
-                    auto-optimise-store = true
-                    experimental-features = nix-command flakes
-                  '';
-
-                  # programs to install
-
-                  programs.neovim = {
-                    enable = true;
-                    viAlias = true;
-                    vimAlias = true;
-                    # extraPackages = [gcc];
-                  };
-
-                  programs.zoxide = {
-                    enable = true; # smarter 'cd'
-                    enableFishIntegration = true;
-                  };
-
-                  programs.bat.enable = true;
-                  programs.fzf.enable = true;
-                  programs.fzf.enableFishIntegration = true;
-                  programs.lsd.enable = true;
-                  programs.lsd.enableAliases = true;
-
-                  programs.fish.enable = true;
-                  programs.fish.shellAliases = {
-                    cd = "z";
-                    zj = "zellij";
-                    lg = "lazygit";
-                    gs = "git status -s";
-                    b = "bat";
-                    n = "nvim";
-                    vim = "nvim";
-                    curl = "curlie";
-                  };
-
-                  # Let Home Manager install and manage itself.
-                  programs.home-manager.enable = true;
-
-                  # configure git
-                  programs.git = {
-                    enable = true;
-                    userName = "Hanz";
-                    userEmail = "haniel56@zoho.eu";
-
-                    ignores = [
-                      ".direnv"
-                    ];
-
-                    extraConfig = {
-                      pull.rebase = true;
-                      init.defaultBranch = "main";
-                      github.user = "fivehanz";
-                    };
-                  };
-
-                  programs.direnv = {
-                    enable = true;
-                    nix-direnv.enable = true;
-                  };
-
-                  programs.starship = let
-                    flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
-                  in {
-                    enable = true;
-                    enableFishIntegration = true;
-
-                    settings =
-                      {
-                        # Other config here
-                        format = "$all"; # Remove this line to disable the default prompt format
-                        palette = "catppuccin_${flavour}";
-                      }
-                      // builtins.fromTOML (builtins.readFile
-                        (pkgs.fetchFromGitHub
-                          {
-                            owner = "catppuccin";
-                            repo = "starship";
-                            rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc"; # Replace with the latest commit hash
-                            sha256 = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
-                          }
-                          + /palettes/${flavour}.toml));
-                  };
-                })
-              ];
-            };
-          }
-        ];
-      };
-
-      mkLinuxVM-aarch64 = inputs.nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          inputs.home-manager.nixosModules.home-manager
-          {
-            # home-manager configs
-          }
-        ];
-      };
+      mkLinuxVM = mkLinux "x86_64-linux";
+      mkLinuxVM-aarch64 = mkLinux "aarch64-linux";
     };
   };
 }
