f98ba5ccef5ead7271fa8e932b95cc54165b1ef8
Hanz <haniel56@zoho.eu>
2024-11-19 03:15:46 +0530

    chore: add sccache

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@
             environment.systemPackages = with pkgs; [
               coreutils
               rustup
+              sccache
               bacon
               ripgrep
               tailscale
